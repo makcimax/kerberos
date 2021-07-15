edf4a48920167352582192ab7156e5ad6db204b3
Dvoranchik <borodin0119@gmail.com>
2021-07-15 10:16:46 +0300

    Залил на старте

---
+++ nsspi-master/NSspi/Contexts/ImpersonationHandle.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Security.Principal;
+using System.Threading;
+
+namespace NSspi.Contexts
+{
+    /// <summary>
+    /// Represents impersonation performed on a server on behalf of a client.
+    /// </summary>
+    /// <remarks>
+    /// The handle controls the lifetime of impersonation, and will revert the impersonation
+    /// if it is disposed, or if it is finalized ie by being leaked and garbage collected.
+    ///
+    /// If the handle is accidentally leaked while operations are performed on behalf of the user,
+    /// impersonation may be reverted at any arbitrary time, perhaps during those operations.
+    /// This may lead to operations being performed in the security context of the server,
+    /// potentially leading to security vulnerabilities.
+    /// </remarks>
+    public class ImpersonationHandle : IDisposable
+    {
+        private readonly ServerContext server;
+
+        private bool disposed;
+
+        /// <summary>
+        /// Initializes a new instance of the ImpersonationHandle. Does not perform impersonation.
+        /// </summary>
+        /// <param name="server">The server context that is performing impersonation.</param>
+        internal ImpersonationHandle( ServerContext server )
+        {
+            this.server = server;
+            this.disposed = false;
+        }
+
+        /// <summary>
+        /// Finalizes the ImpersonationHandle by reverting the impersonation.
+        /// </summary>
+        ~ImpersonationHandle()
+        {
+            Dispose( false );
+        }
+
+        /// <summary>
+        /// Reverts impersonation.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose( true );
+            GC.SuppressFinalize( this );
+        }
+
+        /// <summary>
+        /// Reverts impersonation.
+        /// </summary>
+        /// <param name="disposing">True if being disposed, false if being finalized.</param>
+        private void Dispose( bool disposing )
+        {
+            // This implements a variant of the typical dispose pattern. Always try to revert
+            // impersonation, even if finalizing. Don't do anything if we're already reverted.
+
+            if( this.disposed == false )
+            {
+                this.disposed = true;
+
+                // Just in case the reference is being pulled out from under us, pull a stable copy
+                // of the reference while we're null-checking.
+                var serverCopy = this.server;
+
+                if( serverCopy != null && serverCopy.Disposed == false )
+                {
+                    serverCopy.RevertImpersonate();
+                }
+            }
+        }
+    }
+}
---
+++ nsspi-master/NSspi/EnumMgr.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Reflection;
+
+namespace NSspi
+{
+    /// <summary>
+    /// Tags an enumeration member with a string that can be programmatically accessed.
+    /// </summary>
+    [AttributeUsage( AttributeTargets.Field )]
+    public class EnumStringAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnumStringAttribute"/> class.
+        /// </summary>
+        /// <param name="text">The string to associate with the enumeration member.</param>
+        public EnumStringAttribute( string text )
+        {
+            this.Text = text;
+        }
+
+        /// <summary>
+        /// Gets the string associated with the enumeration member.
+        /// </summary>
+        public string Text { get; private set; }
+    }
+
+    /// <summary>
+    /// Converts betwen enumeration members and the strings associated to the members through the
+    /// <see cref="EnumStringAttribute"/> type.
+    /// </summary>
+    public class EnumMgr
+    {
+        /// <summary>
+        /// Gets the text associated with the given enumeration member through a <see cref="EnumStringAttribute"/>.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string ToText( Enum value )
+        {
+            FieldInfo field = value.GetType().GetField( value.ToString() );
+
+            EnumStringAttribute[] attribs = (EnumStringAttribute[])field.GetCustomAttributes( typeof( EnumStringAttribute ), false );
+
+            if( attribs == null || attribs.Length == 0 )
+            {
+                return null;
+            }
+            else
+            {
+                return attribs[0].Text;
+            }
+        }
+
+        /// <summary>
+        /// Returns the enumeration member that is tagged with the given text using the <see cref="EnumStringAttribute"/> type.
+        /// </summary>
+        /// <typeparam name="T">The enumeration type to inspect.</typeparam>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static T FromText<T>( string text )
+        {
+            FieldInfo[] fields = typeof( T ).GetFields();
+
+            EnumStringAttribute[] attribs;
+
+            foreach( FieldInfo field in fields )
+            {
+                attribs = (EnumStringAttribute[])field.GetCustomAttributes( typeof( EnumStringAttribute ), false );
+
+                foreach( EnumStringAttribute attrib in attribs )
+                {
+                    if( attrib.Text == text )
+                    {
+                        return (T)field.GetValue( null );
+                    }
+                }
+            }
+
+            throw new ArgumentException( "Could not find a matching enumeration value for the text '" + text + "'." );
+        }
+    }
+}
